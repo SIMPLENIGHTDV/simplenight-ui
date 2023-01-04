f196569fe4627d8a6aa222b53e2f15c8e40a0b9f
Isabel Alvarez <isabel@lightit.io>
2023-01-04 12:25:33 -0300

    fixes

--- src/components/Inputs/Select/Select.stories.tsx
+++ src/components/Inputs/Select/Select.stories.tsx
@@ -18,9 +18,18 @@ const selectOptions = [
   { value: '0300', label: '03:00 AM' },
   { value: '0400', label: '04:00 AM' },
   { value: '0500', label: '05:00 AM' },
-  { value: '0600', label: '06:00 AM' }];
+  { value: '0600', label: '06:00 PM' }];
 export const Default = Template.bind({});
 Default.args = {
   options: selectOptions,
   placeholder: 'Select a time',
+  searchable: false,
+  defaultValue: selectOptions[0],
+};
+export const Searchable = Template.bind({});
+Searchable.args = {
+  options: selectOptions,
+  placeholder: 'Select a time',
+  searchable: true,
+
 };
