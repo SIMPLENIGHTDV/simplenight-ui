9371ea3ad9fb5e0b5071333ad9726c1aa1cab186
Daiana Aysa <94717001+daiana-simplenight@users.noreply.github.com>
2023-01-06 17:04:22 -0300

    Add states in doc

--- src/components/Inputs/DateInput/DateInput.stories.tsx
+++ src/components/Inputs/DateInput/DateInput.stories.tsx
@@ -17,4 +17,36 @@ const Template: ComponentStory<typeof DateInput> = (args) => (
 export const Default = Template.bind({});
 Default.args = {
   placeholder: 'Placeholder',
+  value: 'Date',
+};
+
+export const Open = Template.bind({});
+Open.args = {
+  placeholder: 'Placeholder',
+  isOpen: true,
+  value: 'Date',
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  placeholder: 'Placeholder',
+  state: 'error',
+  isOpen: true,
+  value: 'Date',
+};
+
+export const Success = Template.bind({});
+Open.args = {
+  placeholder: 'Placeholder',
+  state: 'success',
+  isOpen: true,
+  value: 'Date',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  placeholder: 'Placeholder',
+  state: 'disabled',
+  isOpen: true,
+  value: 'Date',
 };
