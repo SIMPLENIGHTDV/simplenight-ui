2982acf13b2b41f25f6f2a3ef74f4168ec6bb991
Enrique Franco <enrique@simplenight.com>
2023-01-10 11:08:32 -0400

    feat: create form field

---
+++ src/components/FormField/FormField.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/no-unstable-nested-components */
+import React from 'react';
+import QuestionCirlce from '../../icons/regular/QuestionCircle';
+import Tooltip from '../Tooltip/Tooltip';
+import Label from '../Label/Label';
+
+const FormField = ({ label, required, info, error, counterValue, counterMax, children }: any) => {
+  const Required = () => required && <span className="text-primary-1000">Required</span>;
+  const Error = () => error && <span className="text-sm text-error-1000">{error}</span>;
+  const Counter = () => (counterValue || (counterValue && counterMax)) && (
+    <span className="text-sm text-dark-800">
+      {counterValue}
+      {counterMax && '/'}
+      {counterMax}
+    </span>
+  );
+  return (
+    <section>
+      <section className="flex justify-between mb-1">
+        <section className="flex items-center gap-1">
+          {label && <Label>{label}</Label>}
+          {info && (
+            <Tooltip text="Sec Info">
+              <QuestionCirlce className="text-dark-700 w-3.5" />
+            </Tooltip>
+          )}
+        </section>
+        <Required />
+      </section>
+      {children}
+      <section className="flex justify-between mt-1">
+        <Error />
+        <Counter />
+      </section>
+    </section>
+  );
+};
+
+export default FormField;
