e3b080dd4f565b6dbc2fab405ccdb950408ca92f
Enrique Franco <enrique@simplenight.com>
2023-01-10 12:05:31 -0400

    feat: add question circle tooltip history

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import Tooltip from './Tooltip';
+import QuestionCircle from '../../icons/regular/QuestionCircle';
 
 const title = 'SimplenightUI/';
 
@@ -11,11 +12,25 @@ export default {
   component: Tooltip,
 } as ComponentMeta<typeof Tooltip>;
 
-const Template: ComponentStory<typeof Tooltip> = (args) => <section className="p-8"><Tooltip {...args} /></section>;
+const Template: ComponentStory<typeof Tooltip> = (args) => (
+  <section className="p-8">
+    <Tooltip {...args} />
+  </section>
+);
 
 export const DefaultTooltip = Template.bind({});
+export const QuestionCirlceTooltip = Template.bind({});
 
 DefaultTooltip.args = {
   text: 'Hello World',
   children: <button className="bg-dark-200 p-1 rounded text-dark-700">Hover me</button>,
+};
+
+QuestionCirlceTooltip.args = {
+  text: 'Hello World',
+  children: (
+    <button>
+      <QuestionCircle className="text-dark-700 w-3.5" />
+    </button>
+  ),
 };
