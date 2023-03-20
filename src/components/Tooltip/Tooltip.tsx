148e1e94140b15148f10608f170ccd390ce88788
Isabel Alvarez <isabel@lightit.io>
2023-03-20 12:56:43 -0300

    Implement tooltip on pricing component

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -4,20 +4,20 @@ import IconWrapper from '../IconWrapper';
 import { ITooltip } from './TooltipTypes';
 
 const Tooltip = ({ text, children, position = 'top-left' }: ITooltip) => {
-  const mobilePositionClasses = 'fixed left-[20px] mt-6 ';
+  const mobilePositionClasses = 'fixed left-[20px] mt-6 md:left-auto md:mt-0 ';
   const desktopPositionClasses = {
-    'top-left': 'md:absolute md:bottom-6  md:left-0',
+    'top-left': 'md:absolute md:bottom-6  md:left-0 ',
     'top-right': 'md:absolute  md:bottom-6  md:right-0',
     'top-center': 'md:absolute  md:bottom-6  md:left-1/2 md:-translate-x-1/2',
     'bottom-left': 'md:absolute   md:top-6  md:left-0',
-    'bottom-right': 'md:absolute   md:top-6  md:right-0',
+    'bottom-right': 'md:absolute   md:top-6  md:right-0 ',
     'bottom-center': ' md:absolute  md:top-6  md:left-1/2 md:-translate-x-1/2',
   };
   return (
     <span className="group relative inline-block">
       <span
-        className={`border  opacity-0 group-hover:opacity-100 transition-opacity py-2 px-3 rounded shadow-container
-        w-[calc(100vw-40px)] md:w-max   md:max-w-[335px] capitalize
+        className={`border  text-left opacity-0 group-hover:opacity-100 transition-opacity py-2 px-3 rounded shadow-container
+        w-[calc(100vw-40px)] md:w-max   md:max-w-[335px] capitalize bg-white z-10
          ${mobilePositionClasses} ${desktopPositionClasses[position]}`}
       >
         <Paragraph size="xxsmall">{text}</Paragraph>
